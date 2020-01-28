27df0ed9ed14e65d5b2fec096a04f3d96f75fbae
Thiago Brito <thiago.britos@berghem.com.br>
2020-01-28 16:27:46 -0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='aioadb',
+    version='0.0.1',
+    description='A minimal async adb implementation',
+    keywords=['adb', 'async'],
+    url='https://github.com/tba91/aioadb',
+    author='TBA',
+)
