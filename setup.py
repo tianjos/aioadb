44614ffa4822b834e4019e1663cad4deacc953ed
Thiago Brito <thiago.britos@berghem.com.br>
2020-01-29 16:30:57 -0300

    include setup.cfg and correct setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,22 @@ from setuptools import setup
 
 setup(
     name='aioadb',
+    packages = ['aioadb'],
     version='0.0.1',
+    license='MIT',
     description='A minimal async adb implementation',
+    author='TBA',
+    url = 'https://github.com/tba91/aioadb',
+
     keywords=['adb', 'asyncio'],
     url='https://github.com/tba91/aioadb',
-    author='TBA',
+    download_url='https://github.com/tba91/aioadb/archive/0.0.1.tar.gz'
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+  ],
 )
