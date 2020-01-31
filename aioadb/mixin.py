2aba157a5657a044d64cde1b3a686fdbc6021c61
Thiago Brito <thiago.britos@berghem.com.br>
2020-01-31 17:31:10 -0300

    parser class added, some features in mixin added (wlan ip, write text, swipe, touch), change packages list to be more generic accepting flags

--- aioadb/mixin.py
+++ aioadb/mixin.py
@@ -1,19 +1,36 @@
-from .parser import parser
+from .parser import pre_parser, pos_parser
+
 
 class ShellMixin:
 
     async def _run(self, cmd: str) -> bytes:
         return await self.shell(cmd)
     
-    async def app_start(self, package_name: str):
+    async def app_start(self, package_name: str) -> None:
         await self._run(f'monkey -p {package_name} 1')
-
-    @parser
-    async def list_third_packages(self):
-        res = await self._run('pm list packages -3')
-        return res
     
-    @parser
-    async def resolution_screen(self):
-        screen_size = await self._run(f'wm size')
-        return screen_size
+    @pos_parser
+    async def packages(self, flags: list = None) -> list:
+        if flags is None: flags = []
+        return await self._run(f'pm list packages {" ".join(flags)}')
+    
+    @pos_parser
+    async def resolution_screen(self) -> list:
+        return await self._run('wm size')
+    
+    async def swipe(self, sx, sy, ex, ey: str, duration: int = 400) -> None:
+        await self._run(f'input swipe {sx} {sy} {ex} {ey} {str(duration)}')
+
+    async def touch(self, x, y: str) -> None:
+        await self._run(f'input tap {x} {y}')
+    
+    @pre_parser
+    async def write_text(self, text: str) -> None:
+        await self._run(f'input text {text}')
+    
+    async def key_event(self, key_code: int) -> None:
+        await self._run(f'input keyevent {str(key_code)}')
+    
+    @pos_parser
+    async def wlan_ip(self) -> None:
+        return await self._run(f'ip route')
--- aioadb/parser.py
+++ aioadb/parser.py
@@ -1,8 +1,21 @@
 import abc
+import inspect
+import sys
+import traceback
 from functools import wraps
 
 
-def parser(func):
+def pre_parser(func):
+    @wraps(func)
+    async def wrapper_parser(*args, **kwargs):
+        *rest, arg = args
+        parser_cls = ParserFactory.parsers.get(_lookup_func_name(func), DefaultParser)
+        pre_parse = parser_cls.parse(arg)
+        output = await func(*rest, pre_parse, **kwargs)
+        return output
+    return wrapper_parser
+
+def pos_parser(func):
     @wraps(func)
     async def wrapper_parser(*args, **kwargs):
         parser_cls = ParserFactory.parsers.get(_lookup_func_name(func), DefaultParser)
@@ -20,7 +33,7 @@ class AbstractParser(abc.ABC):
     @abc.abstractmethod
     def parse(cls, output: bytes) -> str:
         pass
-    
+
 
 class DefaultParser(AbstractParser):
 
@@ -36,17 +49,76 @@ class ResolutionScreenParser(AbstractParser):
         return list(map(int, output.decode().split(':')[1].strip().split('x')))
 
 
-class ListThirdPackagesParser(AbstractParser):
+class PackagesParser(AbstractParser):
 
     @classmethod
     def parse(cls, output: bytes) -> list:
-        return list(map(lambda pkg: pkg.split(":")[1], output.decode().split()))
+        try:
+            return list(map(lambda pkg: pkg.split(":")[1], output.decode().split()))
+        except AttributeError:
+            stack = inspect.stack()
+            for frame in stack:
+                if frame.function == 'wrapper_parser':
+                    args = inspect.getargvalues(frame.frame)
+                    kw = args.locals.get('kwargs').values()
+        return f'No packages matching the criteria: {" ".join(*kw)}'
+
+
+class WriteTextParser(AbstractParser):
+
+    @classmethod
+    def parse(cls, text: str) -> str:
+        escaped_text = text.translate(str.maketrans({"-":  r"\-",
+                                                "+":  r"\+",
+                                                "[":  r"\[",
+                                                "]":  r"\]",
+                                                "(":  r"\(",
+                                                ")":  r"\)",
+                                                "{":  r"\{",
+                                                "}":  r"\}",
+                                                "\\": r"\\\\",
+                                                "^":  r"\^",
+                                                "$":  r"\$",
+                                                "*":  r"\*",
+                                                ".":  r"\.",
+                                                ",":  r"\,",
+                                                ":":  r"\:",
+                                                "~":  r"\~",
+                                                ";":  r"\;",
+                                                ">":  r"\>",
+                                                "<":  r"\<",
+                                                "%":  r"\%",
+                                                "#":  r"\#",
+                                                "\'":  r"\\'",
+                                                "\"":  r'\\"',
+                                                "`":  r"\`",
+                                                "!":  r"\!",
+                                                "?":  r"\?",
+                                                "|":  r"\|",
+                                                "=":  r"\=",
+                                                "@":  r"\@",
+                                                "/":  r"\/",
+                                                "_":  r"\_",
+                                                " ":  r"%s", # special
+                                                "&":  r"\&"}))
+        return escaped_text
+
+
+class WlanIpParser:
+
+    @classmethod
+    def parse(cls, output: bytes) -> str:
+        return output.decode().split()[-1]
         
 
 class ParserFactory:
     parsers = {
+        #pos-parsers
         'ResolutionScreen': ResolutionScreenParser,
-        'ListThirdPackages': ListThirdPackagesParser,
+        'Packages': PackagesParser,
+        'WlanIp': WlanIpParser,
+        #pre-parsers
+        'WriteText': WriteTextParser,
         }
 
         
