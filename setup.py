a3fb78599f473ac3d6a374047158034d786083cc
Thiago Brito <thiago.britos@berghem.com.br>
2020-01-31 18:06:10 -0300

    update setup

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     name='aioadb',
     packages = ['aioadb'],
-    version='0.0.1',
+    version='0.1.0',
     license='MIT',
     description='A minimal async adb implementation',
     author='TBA',
     keywords=['adb', 'asyncio'],
     url='https://github.com/tba91/aioadb',
-    download_url='https://github.com/tba91/aioadb/archive/0.0.1.tar.gz',
+    download_url='https://github.com/tba91/aioadb/archive/v0.1.0.tar.gz',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
